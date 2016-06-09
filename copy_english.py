886516e5b5afab5971369849d24143cfe6fce225
Tim Carrell <timncarrell@gmail.com>
2016-06-08 23:54:17 -0700

    Add command line options to English loc export script.

--- copy_english.py
+++ copy_english.py
@@ -1,10 +1,39 @@
-import os
+import os, sys, getopt
 
+#default to exporting from English to all other languages and not overwriting pre-existing files
 allLanguages = {'l_braz_por', 'l_english', 'l_french', 'l_german', 'l_polish', 'l_russian', 'l_spanish'}
 sourceLanguage = 'l_english'
 ignoreLanguages = set([])
 overwrite = False
 
+try:
+	options, extraargs = getopt.getopt(sys.argv[1:], "hi:os:",["help","ignore=","overwrite","source="])
+except getopt.GetoptError:
+	print('Invalid argument syntax in ' + sys.argv[0])
+	print('Valid arguments are -h, --help, -i, --ignore=, -o, --overwrite, -s, --source=')
+	sys.exit(47)
+for opt, val in options:
+	if opt in ("-i", "--ignore"):
+		ignoreLanguages = set(val.split(","))
+		print('Ignoring the following languages:')
+		print(ignoreLanguages)
+	elif opt in ("-h", "--help"):
+		print("-h, --help :")
+		print("    See this help information.")
+		print("-i, --ignore= :")
+		print("    A comma-separated list of languages to not export to")
+		print("    e.g. -i l_braz_por,l_french")
+		print("-o, --overwrite :")
+		print("    If this option is set, will overwrite all localisation files in")
+		print("    non-ignored, non-source languages.")
+		print("-s, --source= :")
+		print("    The language to export from. Default is l_english.")
+	elif opt in ("-o", "--overwrite"):
+		overwrite = True
+		print('Overwriting existing files.')
+	elif opt in ("-s", "--source"):
+		sourceLanguage = val
+		print('Setting source language to ' + val)
 targetLanguages = allLanguages - ignoreLanguages - set([sourceLanguage])
 
 for filename in os.listdir('localisation')[:]:
