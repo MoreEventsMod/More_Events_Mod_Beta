534a24cf0155aed22786a112cf827590d47f53f8
Tim Carrell <timncarrell@gmail.com>
2016-06-30 20:47:09 -0700

    Clean rename event script and add proper directories

--- refactor_events.py
+++ refactor_events.py
@@ -13,66 +13,24 @@
 
 import os, sys, getopt, re
 
-#default to exporting from English to all other languages and not overwriting pre-existing files
-#allLanguages = {'l_braz_por', 'l_english', 'l_french', 'l_german', 'l_polish', 'l_russian', 'l_spanish'}
-#sourceLanguage = 'l_english'
-#ignoreLanguages = set([])
-#overwrite = False
-
 verbose = False
 
 try:
-	#options, extraargs = getopt.getopt(sys.argv[1:], "hi:os:",["help","ignore=","overwrite","source="])
 	options, extraargs = getopt.getopt(sys.argv[1:], "hv",["help", "verbose"])
 except getopt.GetoptError:
 	print('Invalid argument syntax in ' + sys.argv[0])
-	#print('Valid arguments are -h, --help, -i, --ignore=, -o, --overwrite, -s, --source=')
 	print('Valid arguments are -h, --help, -v, --verbose')
 	sys.exit(47)
 for opt, val in options:
 	if opt in ("-h", "--help"):
-		print("help does not yet exist. Sorry :(")
+		print("-h, --help:")
+		print("\tPrint this text and exit.")
+		print("-v", "--verbose")
+		print("\tPrint a message for each substitution made.")
 		sys.exit(0)
 	elif opt in ("-v", "--verbose"):
 		verbose = True
-#for opt, val in options:
-#	if opt in ("-i", "--ignore"):
-#		ignoreLanguages = set(val.split(","))
-#		print('Ignoring the following languages:')
-#		print(ignoreLanguages)
-#	elif opt in ("-h", "--help"):
-#		print("-h, --help :")
-#		print("    See this help information.")
-#		print("-i, --ignore= :")
-#		print("    A comma-separated list of languages to not export to")
-#		print("    e.g. -i l_braz_por,l_french")
-#		print("-o, --overwrite :")
-#		print("    If this option is set, will overwrite all localisation files in")
-#		print("    non-ignored, non-source languages (even if they already exist).")
-#		print("-s, --source= :")
-#		print("    The language to export from. Default is l_english.")
-#		sys.exit(0)
-#	elif opt in ("-o", "--overwrite"):
-#		overwrite = True
-#		print('Overwriting existing files.')
-#	elif opt in ("-s", "--source"):
-#		sourceLanguage = val
-#		print('Setting source language to ' + val)
-#targetLanguages = allLanguages - ignoreLanguages - set([sourceLanguage])
-
-#for filename in os.listdir('localisation')[:]:
-#	sourceFile = open('localisation/' + filename, 'r')
-#	for target in targetLanguages:
-#		newFilename = filename.replace(sourceLanguage, target)
-#		if newFilename == filename: continue	#Only copy files that actually contained l_english
-#		if os.path.isfile('localisation/' + newFilename) and not overwrite: continue
-#		targetFile = open('localisation/' + newFilename, 'w+')
-#		for line in sourceFile:
-#			targetFile.write(line.replace(sourceLanguage + ':', target + ':'))
-#		targetFile.close()
-#		sourceFile.seek(0)
-#print(sourceLanguage + ' localisation export complete.')
-
+		
 sourceNames = [
 	"paul\.(?=[1-6][^0-9])",
 	"paul\.(?=((1[0-9])|(2[01])|(3[0-6])))",
@@ -104,7 +62,7 @@ sourceNames = [
 	"squid\.(?=[1-6][^0-9])",
 	"squid\.(?=7[^0-9])",
 	"squid\.(?=[8-9][^0-9])",
-] #TODO
+]
 goalNames = [
 	"mem-star-survey.",
 	"mem-elusive-carcosa.",
@@ -136,10 +94,17 @@ goalNames = [
 	"mem-eager-traders.",
 	"mem-imperialist-intimidation.",
 	"mem-music-tour.",
-] #TODO
+]
 directories = [
-	"events"
-] #TODO
+	"events",
+	"localisation",
+	"common/anomalies",
+	"common/event_chains",
+	"common/on_actions",
+	"common/opinion_modifiers",
+	"common/static_modifiers",
+	"common/traits",
+]
 
 for src, goal in zip(sourceNames,goalNames):
 	for dir in directories:
