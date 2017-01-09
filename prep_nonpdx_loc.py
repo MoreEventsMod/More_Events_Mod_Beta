acd653cb46bf49f96bf7b864a0b2804755d8d872
Tim Zerrell <tim.zerrell@gmail.com>
2017-01-08 22:35:17 -0800

    Debug prep_nonpdx_loc.py (incomplete)

--- prep_nonpdx_loc.py
+++ prep_nonpdx_loc.py
@@ -53,10 +53,10 @@ if (sourceLanguage == None):
 	print('Defaulting to source language: ' + sourceLanguage)
 
 for filename in os.listdir('localisation')[:]:
-	#TODO: From HERE!!
 	if (filename.count(overwriteLanguage) > 0):
 		continue
 	elif (filename.count(sourceLanguage) == 0 and (not deleteExtraLanguages)):
+		print('DEBUG: Removing unrelated loc file')
 		os.remove('localisation/' + filename)
 		continue
 	sourceFile = open('localisation/' + filename, 'r')
